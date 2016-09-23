acddb709d912344d2d3f9981595e6d71f9d13af9
Steve Goldman <steve.goldman@gmail.com>
2016-09-23 19:04:30 -0400

    parameters from config initializer

---
+++ config/initializers/server.rb
@@ -0,0 +1,4 @@
+DEFAULT_SERVER_URL        = Rails.application.config_for(:server)["server_url"]
+DEFAULT_DONE_DELAY        = Rails.application.config_for(:server)["done_delay"]
+DEFAULT_LOCATE_ME_SECONDS = Rails.application.config_for(:server)["locate_me_seconds"]
+DEFAULT_LOGIN_INTERVAL    = Rails.application.config_for(:server)["login_interval"]
